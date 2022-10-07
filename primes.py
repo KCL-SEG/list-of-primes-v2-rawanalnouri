cbe1a6f69c1e892917d39b951b907a77f292414b
Rawan Alnouri <alnourirawan@gmail.com>
2022-10-07 11:04:34 +0100

    Complete primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes > 0:
+        list = []
+        num = 0
+        while len(list) < number_of_primes:
+            if isPrime(num):
+                list.append(num)
+            num += 1
+        return list
+    else:
+        raise ValueError("Input must be greater than 0.")
+
+def isPrime(num):
+    flag = True
+    if num <= 1:
+        flag = False
+    else:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = False
+                break
+    return flag
